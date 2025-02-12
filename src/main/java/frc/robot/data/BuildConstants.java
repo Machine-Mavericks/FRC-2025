c3de74b0e5480bcee562fcb101f9a34f88d37b9a
superzokabear <superzokabear@gmail.com>
2025-02-11 19:28:04 -0500

    ?

--- src/main/java/frc/robot/data/BuildConstants.java
+++ src/main/java/frc/robot/data/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot.data;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC-2025";
+  public static final String MAVEN_NAME = "FRC-2025-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 26;
-  public static final String GIT_SHA = "dd5b2c3c5153d5204c7dc5b39191a07390e74932";
-  public static final String GIT_DATE = "2025-02-10 20:36:31 EST";
+  public static final int GIT_REVISION = 32;
+  public static final String GIT_SHA = "52ab9fe5661baed3d12247378934a1304311f564";
+  public static final String GIT_DATE = "2025-02-11 17:40:04 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-10 20:54:08 EST";
-  public static final long BUILD_UNIX_TIME = 1739238848583L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-11 19:21:08 EST";
+  public static final long BUILD_UNIX_TIME = 1739319668474L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
