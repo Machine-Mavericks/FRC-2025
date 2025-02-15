f7b41a1ec02fcf23460e4cebf2d46db85bb09217
superzokabear <superzokabear@gmail.com>
2025-02-14 21:21:16 -0500

    elevator

--- src/main/java/frc/robot/data/BuildConstants.java
+++ src/main/java/frc/robot/data/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot.data;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC-2025";
+  public static final String MAVEN_NAME = "FRC-2025-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 26;
-  public static final String GIT_SHA = "dd5b2c3c5153d5204c7dc5b39191a07390e74932";
-  public static final String GIT_DATE = "2025-02-10 20:36:31 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-10 20:54:08 EST";
-  public static final long BUILD_UNIX_TIME = 1739238848583L;
+  public static final int GIT_REVISION = 42;
+  public static final String GIT_SHA = "636c59fc47fed078ed7d42fdb82ede187fd98371";
+  public static final String GIT_DATE = "2025-02-14 19:18:57 EST";
+  public static final String GIT_BRANCH = "Elevator";
+  public static final String BUILD_DATE = "2025-02-14 21:19:24 EST";
+  public static final long BUILD_UNIX_TIME = 1739585964466L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -20,7 +22,8 @@ public class Elevator extends SubsystemBase {
     static double gearCircumference = gearDiameterCM * Math.PI;
     static double ticksPerRev = 42;
     static double lastPosTicks;
-    static double low = 81.0, med = 121.0, high = 183.0, station = 95.0; 
+    public static double low = 81.0, med = 121.0, high = 183.0, trough = 95.0; 
+    private static double ELEVATOR_SPEED = 1; 
     // Local objects and variables here
     // These are for things that only belong to, and used by, the subsystem
 
@@ -34,16 +37,17 @@ public class Elevator extends SubsystemBase {
         elevatorConfig.idleMode(IdleMode.kBrake);
         elevatorConfig.inverted(false);
         elevatorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder);
-        elevatorConfig.closedLoop.p(0.0);
+        elevatorConfig.closedLoop.p(104.0);
         elevatorConfig.closedLoop.i(0.0);
         elevatorConfig.closedLoop.d(0.0);
-        elevatorConfig.closedLoop.outputRange(-1.0, 1.0);
+        elevatorConfig.closedLoop.outputRange(-0.6, 0.6);
         elevatorConfig.closedLoop.positionWrappingEnabled(false);
         elevatorMotorL.configure(elevatorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         SparkMaxConfig followConfig = new SparkMaxConfig();
         followConfig.follow(elevatorMotorL, false);
         elevatorMotorR.configure(followConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         lastPosTicks = 0.0;
+        
     }
 
     /**
@@ -56,9 +60,9 @@ public class Elevator extends SubsystemBase {
     }
 
     public void moveToPosition(Double position) {
-        cmToTicks(position);
-        
+        cmToTicks(position);      
     }
+    
     private double cmToTicks(double cm){
         return cm/(gearCircumference/ticksPerRev);
         //8.43 - 1 gear ratio
@@ -66,6 +70,10 @@ public class Elevator extends SubsystemBase {
         // 42 ticks/revolution(from spec sheet)
     }
 
+    public void elevatorRun(double state) {
+        elevatorMotorR.getClosedLoopController().setReference(state, ControlType.kPosition, ClosedLoopSlot.kSlot0);
+        elevatorMotorL.getClosedLoopController().setReference(state, ControlType.kPosition, ClosedLoopSlot.kSlot0);
+      }
     // place special subsystem methods here
     // this is where rest of program can access functions to return
     // values or control the subsystem
