f7a2684ef8a0122f01b4a39eb175114e250c5df8
kaitlyn <cardosok@limestone.on.ca>
2025-02-26 18:47:30 -0500

    ?

--- src/main/java/frc/robot/data/BuildConstants.java
+++ src/main/java/frc/robot/data/BuildConstants.java
@@ -7,21 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-2025";
   public static final String VERSION = "unspecified";
-<<<<<<< HEAD
-  public static final int GIT_REVISION = 101;
-  public static final String GIT_SHA = "cf09ae58ca997e535c0b09a8de48a41847348ac6";
-  public static final String GIT_DATE = "2025-02-25 19:08:50 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-25 19:50:57 EST";
-  public static final long BUILD_UNIX_TIME = 1740531057494L;
-=======
   public static final int GIT_REVISION = 103;
   public static final String GIT_SHA = "1ad1ab9ca9e9b226c8cd549fa8303a31cd61a377";
   public static final String GIT_DATE = "2025-02-25 19:24:01 EST";
   public static final String GIT_BRANCH = "main";
   public static final String BUILD_DATE = "2025-02-25 19:59:28 EST";
   public static final long BUILD_UNIX_TIME = 1740531568565L;
->>>>>>> b03823f85b4907cd9fb2be79e3e731bc51ba9486
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
