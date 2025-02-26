cf09ae58ca997e535c0b09a8de48a41847348ac6
superzokabear <superzokabear@gmail.com>
2025-02-25 19:08:50 -0500

    fixing odometry

--- src/main/java/frc/robot/data/BuildConstants.java
+++ src/main/java/frc/robot/data/BuildConstants.java
@@ -7,23 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-2025-4";
   public static final String VERSION = "unspecified";
-<<<<<<< HEAD
-  public static final int GIT_REVISION = 93;
-  public static final String GIT_SHA = "39ab0e6a3317b8e2bac2e6e97495ba6b6b0831be";
-  public static final String GIT_DATE = "2025-02-24 20:18:18 EST";
+  public static final int GIT_REVISION = 100;
+  public static final String GIT_SHA = "a79392ccff449422e5cc7576cb26b61ddda3d8a3";
+  public static final String GIT_DATE = "2025-02-25 18:30:07 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-25 18:21:03 EST";
-  public static final long BUILD_UNIX_TIME = 1740525663776L;
-  public static final int DIRTY = 0;
-=======
-  public static final int GIT_REVISION = 94;
-  public static final String GIT_SHA = "feb1d6e254691897e924f533ca3e65b03c4e1653";
-  public static final String GIT_DATE = "2025-02-24 19:44:13 EST";
-  public static final String GIT_BRANCH = "NeoBase(Optional)";
-  public static final String BUILD_DATE = "2025-02-25 18:19:53 EST";
-  public static final long BUILD_UNIX_TIME = 1740525593860L;
+  public static final String BUILD_DATE = "2025-02-25 18:56:38 EST";
+  public static final long BUILD_UNIX_TIME = 1740527798332L;
   public static final int DIRTY = 1;
->>>>>>> 27d951cbb210d2449ba55bc8c166984b05f0e79e
 
   private BuildConstants(){}
 }
