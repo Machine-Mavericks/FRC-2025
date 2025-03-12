953d383fd85dd3241a52478615e9e82b6c4c3530
superzokabear <superzokabear@gmail.com>
2025-03-12 17:31:47 -0400

    new two coral auto

--- src/main/java/frc/robot/data/BuildConstants.java
+++ src/main/java/frc/robot/data/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 160;
-  public static final String GIT_SHA = "898c203f5317608dcd04893705ebe814ac1a6b7f";
-  public static final String GIT_DATE = "2025-03-11 11:29:34 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-11 22:19:42 EDT";
-  public static final long BUILD_UNIX_TIME = 1741745982640L;
+  public static final int GIT_REVISION = 161;
+  public static final String GIT_SHA = "83bfc2a3be119b2bf38ccffa3925b08f7fb364d4";
+  public static final String GIT_DATE = "2025-03-11 22:23:05 EDT";
+  public static final String GIT_BRANCH = "HEAD";
+  public static final String BUILD_DATE = "2025-03-12 17:20:04 EDT";
+  public static final long BUILD_UNIX_TIME = 1741814404501L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
